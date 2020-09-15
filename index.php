c0693497913525500843f83961d7b94d9a020846
alibabayev2002 <acemiyt23@gmail.com>
2020-09-16 02:03:05 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+    function explode_($string,$search){
+        $array = [];
+        $index = [];
+        for($i = 0 ; $i < strlen($string) ; $i++){
+            $bool = true;
+            for($j = 0; $j < strlen($search) ; $j++){
+                if($string[$i + $j] != $search[$j]){
+                    $bool = false;
+                    break;
+                }
+            }
+            if($bool){
+                $index[] = $i;
+            }
+        }
+        $k = 0;
+        for($l = 0; $l < count($index) ; $l++){
+            if($k!=0){
+                $k+=strlen($search);
+            }
+            $array[] = substr($string,$k,$index[$l]-$k);
+            $k = $index[$l];
+            if(count($index)-1==$l){
+                $array[] = substr($string,$index[$l]+strlen($search),strlen($string));
+            }
+        }
+    return $array;
+    }
+    
+    print_r(explode__("salam,salam,got",","));
+
+
+?>
